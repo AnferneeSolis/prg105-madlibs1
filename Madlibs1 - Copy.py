d3ef6d73880ab824344eb5e377d14cdfe745fca8
AnferneeSolis <asolis@students.mchenry.edu>
2016-08-28 21:18:08 -0500

    Add files via upload

---
+++ Madlibs1 - Copy.py
@@ -0,0 +1,50 @@
+# Variables
+noun1 = "dog"
+verb1 = "sang"
+verb2 = "run"
+verb3 = "talk"
+noun2 = "airplanes"
+location = "temple"
+noun3 = "legos"
+noun4 = "parrot"
+noun5 = "dragon"
+verb4 = "laugh"
+
+# poem
+print("As a " + noun1 + "you would wait")
+print("And watch from far away.")
+print("But you always knew you'd be the one")
+print("that " + verb1 + " while they all play.\n")
+
+print("In youth you'd " + verb2)
+print("Awake at night and " + verb3)
+print("of all the things that you would change,")
+print("But it was just a dream!\n")
+
+print("Here we are, don't turn away now,")
+print("We are the" + noun2 + " that built this " + location + ".")
+print("Here we are, don't turn away now,")
+print("We are the" + noun2 + " that built this " + location + ".")
+print("From " + noun3 + ".\n")
+
+print("The Time will come")
+print("When you'll have to rise")
+print("Above the rest and prove yourself,")
+print("Your" + noun4 + " never dies.\n")
+
+print("Farewell, I've gone to take my " + noun5 + " above,")
+print("But don't " + verb4 + " for me")
+print("'cause this will be")
+print("the labor of my love.\n")
+
+print("Here we are, don't turn away now,")
+print("We are the" + noun2 + " that built this " + location + ".")
+print("Here we are, don't turn away now,")
+print("We are the" + noun2 + " that built this " + location + ".")
+print("From " + noun3 + ".\n")
+
+print("Here we are, don't turn away now,")
+print("We are the" + noun2 + " that built this " + location + ".")
+print("Here we are, don't turn away now,")
+print("We are the" + noun2 + " that built this " + location + ".")
+print("From " + noun3 + ".")
