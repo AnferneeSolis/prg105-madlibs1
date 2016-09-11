61eb9f9fc17f5cdca60c05069c1b4753328721dd
AnferneeSolis <asolis@students.mchenry.edu>
2016-09-11 16:09:27 -0500

    Update Madlibs1 - Copy.py

--- Madlibs1 - Copy.py
+++ Madlibs1 - Copy.py
@@ -1,17 +1,16 @@
-# Variables
-noun1 = "dog"
-verb1 = "sang"
-verb2 = "run"
-verb3 = "talk"
-noun2 = "airplanes"
-location = "temple"
-noun3 = "legos"
-noun4 = "parrot"
-noun5 = "dragon"
-verb4 = "laugh"
+noun1 = raw_input("linving thing")
+verb1 = raw_input("action")
+verb2 = raw_input("verb")
+verb3 = raw_input("verb")
+noun2 = raw_input("noun")
+location = raw_input("location")
+noun3 = raw_input("noun")
+noun4 = raw_input("noun")
+noun5 = raw_input("noun")
+verb4 = raw_input("verb")
 
-# poem
-print("As a " + noun1 + "you would wait")
+
+print("As a(n) " + noun1 + " you would wait")
 print("And watch from far away.")
 print("But you always knew you'd be the one")
 print("that " + verb1 + " while they all play.\n")
@@ -22,15 +21,15 @@ print("of all the things that you would change,")
 print("But it was just a dream!\n")
 
 print("Here we are, don't turn away now,")
-print("We are the" + noun2 + " that built this " + location + ".")
+print("We are the " + noun2 + " that built this " + location + ".")
 print("Here we are, don't turn away now,")
-print("We are the" + noun2 + " that built this " + location + ".")
+print("We are the " + noun2 + " that built this " + location + ".")
 print("From " + noun3 + ".\n")
 
 print("The Time will come")
 print("When you'll have to rise")
 print("Above the rest and prove yourself,")
-print("Your" + noun4 + " never dies.\n")
+print("Your " + noun4 + " never dies.\n")
 
 print("Farewell, I've gone to take my " + noun5 + " above,")
 print("But don't " + verb4 + " for me")
@@ -38,13 +37,13 @@ print("'cause this will be")
 print("the labor of my love.\n")
 
 print("Here we are, don't turn away now,")
-print("We are the" + noun2 + " that built this " + location + ".")
+print("We are the " + noun2 + " that built this " + location + ".")
 print("Here we are, don't turn away now,")
-print("We are the" + noun2 + " that built this " + location + ".")
+print("We are the " + noun2 + " that built this " + location + ".")
 print("From " + noun3 + ".\n")
 
 print("Here we are, don't turn away now,")
-print("We are the" + noun2 + " that built this " + location + ".")
+print("We are the " + noun2 + " that built this " + location + ".")
 print("Here we are, don't turn away now,")
-print("We are the" + noun2 + " that built this " + location + ".")
+print("We are the " + noun2 + " that built this " + location + ".")
 print("From " + noun3 + ".")
